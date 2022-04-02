7c4bce0e1c31b18610aa99d7c43ab85791671d0c
laripeanuts <larissarabelolf@gmail.com>
2022-04-02 14:35:53 -0300

    Add nav links

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -4,7 +4,10 @@ export const Navigation = () => {
   return (
     <>
       <Link to="/">HOME</Link>
+      <Link to="/login">LOGIN</Link>
       <Link to="/articles">MEUS ARTIGOS</Link>
       <Link to="/articles/new">NOVO ARTIGO</Link>
+      <Link to="/">LOGOUT</Link>
+    </>
   );
 };
