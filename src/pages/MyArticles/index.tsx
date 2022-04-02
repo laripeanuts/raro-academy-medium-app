2f781e6e6b2fc4b62c0f00c95ff36647b8f5df93
laripeanuts <larissarabelolf@gmail.com>
2022-04-02 20:50:20 -0300

    comunicação com API

--- src/pages/MyArticles/index.tsx
+++ src/pages/MyArticles/index.tsx
@@ -1,27 +1,28 @@
-import axios from "axios";
+
 import { useEffect, useState } from "react";
+ import apiClient from "../../services/api-client";
 
 import { ArticleList } from "../../components/ArticleList";
 import { ArticleThumbnailProps } from "../../components/ArticleThumbnail/ArticleThumbnail.types";
 import { geraArtigos } from "../../stories/helpers/gerador-artigos";
 
 export const MyArticlesPage = () => {
-  const [articles, setArticles] = useState<ArticleThumbnailProps[]>([]);
-  
+
   async function buscaMeusArtigos() {
-    const response = await axios.get<ArticleThumbnailProps[]>(
-      "http://3.221.159.196:3307/artigos/meus-artigos"
+    const token = localStorage.getItem("access_token");
+
+    const response = await apiClient.get<ArticleThumbnailProps[]>(
+      "/articles/my-articles"
     );
+
     setArticles(response.data);
   }
 
-  useEffect(() => {
-    buscaMeusArtigos();
-  }, []);
+  const [articles, setArticles] = useState<ArticleThumbnailProps[]>([]);
 
   useEffect(() => {
     setArticles(
-      geraArtigos(5).map((article) => ({ ...article, editavel: true }))
+      geraArtigos(5).map((artigo) => ({ ...artigo, editavel: true }))
     );
   }, []);
 
