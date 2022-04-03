2e15e198fb430f1c5351c1b1fdd96dd3b4500a82
laripeanuts <larissarabelolf@gmail.com>
2022-04-03 14:53:57 -0300

    implementa lista de artigo

--- src/pages/Article/index.tsx
+++ src/pages/Article/index.tsx
@@ -1,20 +1,37 @@
 import { useEffect, useState } from "react";
 import { ArticleView } from "../../components/ArticleView";
-import faker from "@faker-js/faker";
+import { useParams } from "react-router-dom";
+import { ArticleThumbnailProps } from "../../components/ArticleThumbnail/ArticleThumbnail.types";
+import apiClient from "../../services/api-client";
 
 export const ArticlePage = () => {
-  const [article, setArticle] = useState<string>("");
-  const [autor] = useState({
-    nome: faker.name.firstName(),
-    avatar: faker.image.avatar(),
+  const [article, setArticle] = useState<ArticleThumbnailProps>({
+    id: 0,
+    imagem: "",
+    titulo: "",
+    resumo: "",
+    dataPublicacao: new Date(),
+    tempoLeitura: "",
+    conteudo: "",
+    editavel: false,
+    autor: {
+      nome: "",
+      avatar: "",
+      id: 0,
+    },
+  });
+  const [autor, setAutor] = useState({
+    nome: article.autor.nome,
+    avatar: article.autor.avatar,
   });
   const [dataPublicacao] = useState(new Date());
-
+  const { id } = useParams();
+k
   useEffect(() => {
     async function loadArticle() {
-      const response = await fetch("/article.md");
-      const article = await response.text();
-      setArticle(article);
+      const response = await apiClient.get<ArticleThumbnailProps>(`/artigos/${id}`);
+      setArticle(response.data);
+      setAutor(response.data.autor);
     }
 
     loadArticle();
@@ -23,7 +40,7 @@ export const ArticlePage = () => {
   return (
     <div className="m-10">
       <ArticleView
-        article={article}
+        article={article.conteudo}
         autor={autor}
         dataPublicacao={dataPublicacao}
         tempoLeitura={"10min"}
--- src/pages/Articles/index.tsx
+++ src/pages/Articles/index.tsx
@@ -2,16 +2,25 @@
 import { useEffect, useState } from "react";
 import { ArticleList } from "../../components/ArticleList";
 import { ArticleThumbnailProps } from "../../components/ArticleThumbnail/ArticleThumbnail.types";
-import { geraArtigos } from "../../stories/helpers/gerador-artigos";
+import apiClient from "../../services/api-client";
 
 export const ArticlesPage = () => {
   const [articles, setArticles] = useState<ArticleThumbnailProps[]>([]);
+  const [isLoading, SetIsLoading] = useState(true);
 
+  async function buscaArtigos() {
+    const response = await apiClient.get<ArticleThumbnailProps[]>(
+      `/artigos`
+    );
+    setArticles(response.data);
+    SetIsLoading(false);
+  }
   useEffect(() => {
-    setArticles(geraArtigos(10));
+    buscaArtigos();
   }, []);
 
   return (
+    isLoading ? <div></div> :
     <div className="my-30">
       <ArticleList articles={articles} />
     </div>
